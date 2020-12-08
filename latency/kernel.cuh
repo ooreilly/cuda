58d9313274a1e24dbea8a2cb2a851d579e7a5ac0
Ossian O'Reilly <ossian.oreilly@gmail.com>
2020-12-08 00:03:09 -0800

    account for delay in latency program.

--- latency/kernel.cuh
+++ latency/kernel.cuh
@@ -17,18 +17,22 @@ __global__ void measure_latency(int *arr, int j, int niter, int *cpi) {
         if (idx > 32) arr[0] = j;
 
 
+        // estimated delay caused by clock overhead
+        int delay = 18;
+        clock_t start, stop;
         #pragma unroll(unroll_factor)
         for (int it = 0; it < niter; it++) {
-                        clock_t start = clock();
+                        start = clock();
                         j = arr[j];
-                        clock_t stop = clock();
+                        stop = clock();
+
                         if (stop > start) {
                                 s_cpi[0] += stop - start;
                                 total_iter++;
                         }
         }
 
-        cpi[0] = s_cpi[0] / total_iter;
+        cpi[0] = s_cpi[0] / total_iter - delay;
         if (idx > 32) arr[0] = j;
 }
 
